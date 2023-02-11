2b547006a56ae4b2cf226b0ebb6774feb00010fd
wox0 <dawid.woznica@pm.me>
2023-02-11 12:48:41 +0100

    Bullet pool extracted from BurstTower

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AFSInterview
+{
+    public class BulletPool<T> where T : MonoBehaviour
+    {
+        private readonly GameObject bulletPrefab;
+        private readonly List<T> pooledBullets;
+
+        public BulletPool(int initialBulletAmount, GameObject bulletPrefab)
+        {
+            this.bulletPrefab = bulletPrefab;
+            
+            pooledBullets = new List<T>();
+            
+            for (var i = 0; i < initialBulletAmount; i++)
+            {
+                CreateNewBullet();
+            }
+        }
+
+        private T CreateNewBullet()
+        {
+            var bullet = Object.Instantiate(bulletPrefab).GetComponent<T>();
+            bullet.gameObject.SetActive(false);
+            pooledBullets.Add(bullet);
+            return bullet;
+        }
+
+        public T GetBullet()
+        {
+            foreach (var pooledObject in pooledBullets)
+            {
+                if(!pooledObject.gameObject.activeInHierarchy)
+                    return pooledObject;
+            }
+
+            return CreateNewBullet();
+        }
+    }
+}
